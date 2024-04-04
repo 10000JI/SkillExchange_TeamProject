902942b457bd5815710373d54a5d8122ad91cdce
10000JI <sksk436@naver.com>
2024-04-04 17:06:23 +0900

    요일 정렬

--- backend/src/main/java/place/skillexchange/backend/common/util/DayOfWeekUtil.java
+++ backend/src/main/java/place/skillexchange/backend/common/util/DayOfWeekUtil.java
@@ -15,10 +15,7 @@ public class DayOfWeekUtil {
     }
 
     public static Set<String> convertSelectedDaysToString(Set<DayOfWeek> selectedDays) {
-        // 요일을 순서대로 정렬하기 위한 배열
         String[] daysOfWeekInOrder = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-
-        // 정렬된 문자열 집합 생성
         Set<String> sortedDays = new LinkedHashSet<>();
         for (String  day : daysOfWeekInOrder) {
             if (selectedDays.contains(DayOfWeek.valueOf(day))) {
--- backend/src/main/java/place/skillexchange/backend/talent/entity/DayOfWeek.java
+++ backend/src/main/java/place/skillexchange/backend/talent/entity/DayOfWeek.java
@@ -2,7 +2,6 @@ package place.skillexchange.backend.talent.entity;
 
 public enum DayOfWeek {
     SUN, MON, TUE, WED, THU, FRI, SAT;
-
     public static DayOfWeek fromString(String day) {
         return DayOfWeek.valueOf(day);
     }
